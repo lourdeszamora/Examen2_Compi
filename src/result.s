a5ded0adc75566349986266fdb5ef5426c510737
carlos.varela1 <carlos.varela@grupoleitz.com>
2021-12-14 17:07:39 -0600

    Final changes

---
+++ src/result.s
@@ -0,0 +1,12 @@
+.data
+
+.globl main
+.text
+Assignation statement code generation
+Assignation statement code generation
+Print statement code generation
+Print statement code generation
+Print statement code generation
+Print statement code generation
+Print statement code generation
+
